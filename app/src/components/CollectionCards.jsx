48557585c7fb58a6a9306d30768941424c8827f0
patgarcia <commonzenmedia@gmail.com>
2021-05-21 13:04:04 -0400

    Collections layout improvements

--- app/src/components/CollectionCards.jsx
+++ app/src/components/CollectionCards.jsx
@@ -1,24 +1,25 @@
-import React, {useState, useEffect, useContext} from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { DataContext } from './DataContext';
-import CollectionCard from './CollectionCard'
+import CollectionCard from './CollectionCard';
 
 //Material UI Imports
-import { Grid } from "@material-ui/core";
+import { Grid } from '@material-ui/core';
 
-
-function CollectionCards({data}) {
-    const {search, setSearch} = useContext(DataContext)
+function CollectionCards({ data }) {
+    const { search, setSearch } = useContext(DataContext);
     return (
         <Grid container spacing={2}>
-            {data ? 
-            (data.filter(coll => coll.tag.name.includes(search)).map(coll => {
-                return (
-                    <Grid item xs={12} sm={4}>
-                        <CollectionCard data={coll} key={coll._id}/>
-                    </Grid>
-                )
-            }))
-            : "No data"}
+            {data
+                ? data
+                      .filter(coll => coll.tag.name.includes(search))
+                      .map(coll => {
+                          return (
+                              <Grid key={coll._id} item xs={12} sm={4}>
+                                  <CollectionCard data={coll} />
+                              </Grid>
+                          );
+                      })
+                : 'No data'}
         </Grid>
     );
 }
